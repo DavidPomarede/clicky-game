949602dbf5e236065b0e90c366bd401f4f524653
davidpom <davidpom@gmail.com>
2019-09-21 00:15:46 -0400

    Updates

---
+++ precache-manifest.f857f70150c03689c4bd520d0b61e4f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59bc4b93b24ec70e5497896bc7bb10a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "69b503a3c2c14109712b",
+    "url": "/clicky-game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "9fe07070ce9ab8fcb294",
+    "url": "/clicky-game/static/js/2.e3298f3e.chunk.js"
+  },
+  {
+    "revision": "69b503a3c2c14109712b",
+    "url": "/clicky-game/static/js/main.48b9abc1.chunk.js"
+  },
+  {
+    "revision": "f8078f3713d81e8b7938",
+    "url": "/clicky-game/static/js/runtime-main.b7b3959a.js"
+  }
+]);
